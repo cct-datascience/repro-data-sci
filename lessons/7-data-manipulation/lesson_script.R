935dc1dc57f84aef1fb6fc1ae1d3f5b93787c923
Renata Diaz <renatadiaz@arizona.edu>
2024-09-06 06:39:14 -0700

    Working on scripts.

--- lessons/7-data-manipulation/lesson_script.R
+++ lessons/7-data-manipulation/lesson_script.R
@@ -1,10 +1,10 @@
 # Create a new branch for this session's work
 
-# Create a new script, call it "06-data-manip.R"
+# Create a new script, call it "07-data-manip.R"
 
 # Install dplyr and tidyr
 
-if(FALSE){
+if (FALSE) {
   install.packages("dplyr")
   install.packages("tidyr")
   install.packages("palmerpenguins")
@@ -21,14 +21,13 @@ penguins_data <- penguins
 # Create a directory to store the raw data
 # Store the raw data there
 
-write.csv(penguins_data, "06-data/data_raw/penguins_data_raw.csv", row.names = F)
+write.csv(
+  penguins_data,
+  "lessons/7-data-manipulation/data_raw/penguins_data_raw.csv",
+  row.names = F
+)
 
-#### Tibbles & data frames ####
-
-class(penguins_data) 
-# it's a tibble AND a data.frame
-# talk about how R stores data in a data.frame
-# and that tibbles are specific to the tidyverse 
+#### Looking at the penguins data ####
 
 penguins_data
 head(penguins_data)
@@ -40,75 +39,107 @@ str(penguins_data)
 # note factors
 # note presence of NAs
 
-##### Data types #####
-
-# vectors
-
-# integer
-flippers <- penguins_data$flipper_length_mm
-flippers
-mode(flippers)
-
-# numeric
-weights <- penguins_data$body_mass_g
-weights
-mode(weights)
-
-# FACTOR
-
-species <- penguins_data$species
-species
-mode(species)
-class(species)
-
-# character
-
-species_chr <- as.character(penguins_data$species)
-species_chr
-mode(species_chr)
-class(species_chr)
-
-# talk about, maybe demo logical
-
-# create vectors
-# show that vectors must be all one type
-# show that R will convert to chr if mixed
-
-##### Operations on vectors #####
-
-# show summary-style operations
-mean(penguins_data$year)
-
-# show vectorized operations
-
-paste("Year: ", penguins_data$year)
-
-##### Missing data #####
-
-# introduce concept of NA
-# demo performing operation on a column with NAs
-# demo na.rm = T
-
-mean(penguins_data$bill_length_mm)
-mean(penguins_data$bill_length_mm, na.rm = T)
-
-#### Data manipulation #### 
-
-##### Subsetting and filtering data #####
+#### Data manipulation ####
 
 ##### Assignment and pipes #####
 
+# We assign values to variable names using the <-
+# The = also works in *most* instances
+# We use pipes to chain multiple operations together.
+# Keyboard shortcuts: look up in Tools -> Keyboard Shortcuts Help
+
+##### Subsetting and filtering data #####
+
+penguin_locations <- penguins |>
+  select(species, island)
+
+penguin_measures <- penguins |>
+  select(species, ends_with("mm"))
+
+adelie_penguins <- penguins |>
+  filter(species == "Adelie")
+
 ##### Adding columns (mutate) #####
+
+penguins_ratio <- penguins |>
+  mutate(bill_length_depth_ratio = bill_length_mm / bill_depth_mm)
+
+penguins_rounded <- penguins |>
+  mutate(bill_length_mm_rounded = round(bill_length_mm, digits = 0))
+
+penguins_rounded <- penguins |>
+  mutate(across(ends_with("mm"), round))
+
+penguins_rounded <- penguins |>
+  mutate(across(ends_with("mm"), round, .names = "{.col}_rounded"))
 
 ##### Split-apply-combine #####
 
+penguin_summary <- penguins |>
+  group_by(island, species) |>
+  summarize(total_penguins = n(),
+            total_penguin = sum(body_mass_g)) |>
+  ungroup()
+
+penguin_totals <- penguins |>
+  group_by(island, species) |>
+  summarize(total_penguins = n(),
+            total_penguin = sum(body_mass_g, na.rm = T)) |>
+  ungroup()
+
+penguin_summary <- penguins |>
+  group_by(island, species, sex) |>
+  summarize(across(ends_with("mm"), mean)) |>
+  ungroup()
+
+penguin_summary <- penguins |>
+  group_by(island, species, sex) |>
+  summarize(across(ends_with("mm"), \(x) mean(x, na.rm = T))) |>
+  ungroup()
+
 ##### Reshaping #####
 
-#### Optional: advanced topics ####
+penguins_site_by_species <- penguin_totals |>
+  select(species, island, total_penguins)
 
-##### reprex #####
+penguins_site_by_species <- penguin_totals |>
+  select(species, island, total_penguins) |>
+  tidyr::pivot_wider(id_cols = island,
+                     names_from = species,
+                     values_from = total_penguins)
+
+penguins_site_by_species <- penguin_totals |>
+  select(species, island, total_penguins) |>
+  tidyr::pivot_wider(
+    id_cols = island,
+    names_from = species,
+    values_from = total_penguins,
+    values_fill =  0
+  )
+
+penguins_back_to_totals <- penguins_site_by_species |>
+  tidyr::pivot_longer(-island, names_to = "Species", values_to = "Abundance")
 
 ##### joins + advanced joins #####
+
+island_coordinates <- data.frame(
+  island = c("Biscoe", "Dream", "Torgersen"),
+  latitude = c(-65.433, -64.733, -64.766),
+  longitude = c(-65.5, -64.344, -64.083)
+)
+library(ggplot2)
+
+ggplot(island_coordinates, aes(latitude, longitude)) +
+  geom_point() +
+  geom_text(aes(label = island), nudge_x = .07)
+
+penguins_coords <- left_join(penguins, island_coordinates)
+
+ggplot(penguins_coords, aes(longitude, body_mass_g, color = species)) +
+  geom_jitter() +
+  scale_color_viridis_d(option = "mako", begin = .2, end = .8)
+
+#### Optional: advanced topics ####
 
 #### Modify - add - commit ####
 
