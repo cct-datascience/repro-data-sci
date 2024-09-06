5e615beb6959eed769b6e9ca7807ede717c9be79
Renata Diaz <renatadiaz@arizona.edu>
2024-09-06 13:54:43 -0700

    Feels like there should be more to cover here.

--- lessons/9-intermediate-r-2/lesson_script.R
+++ lessons/9-intermediate-r-2/lesson_script.R
@@ -3,11 +3,68 @@ library(palmerpenguins)
 
 ### Iteration with  for loops
 
+#### Basic iteration
+for(i in 1:10) {
+  print(i)
+}
+
+#### Operation
+for(i in 1:10) {
+  print(i^2)
+}
+
+#### Iteration just needs a vector
+for(letter in c("a", "b", "c")) {
+  print(letter)
+}
+
+#### Iteration to modify things
+my_vector <- 1:10
+
+for(i in 1:length(my_vector)) {
+  my_vector[i] <- my_vector[i]^2
+}
+
+# Note this can be slow and isn't preferred unless it's your only option!
+
+
 ### Iteration with purrr
 
 library(purrr)
 
+purrr::map(1:10, \(x) x)
+
+purrr::map(1:10, \(x) x^2)
+
+
 ### Iteration with more complex workflows
+
+#### For loop
+
+for(an_island in c("Biscoe", "Dream")) {
+  
+  island_data <- filter(penguins,
+                        island == an_island)
+  
+  print(summary(lm(bill_length_mm ~ species, data = island_data)))
+  
+}
+
+# Store output in a list
+
+island_lms <- list()
+
+for(an_island in c("Biscoe", "Dream")) {
+  
+  island_data <- filter(penguins,
+                        island == an_island)
+  
+  island_lms[[an_island]] <- summary(lm(bill_length_mm ~ species, data = island_data))
+
+}
+
+#### purrr
+
 
 ### purrr cheatsheet: https://github.com/rstudio/cheatsheets/blob/main/purrr.pdf
 
