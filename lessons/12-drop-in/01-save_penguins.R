7e9c2e933334291686b8f125538fdea534da27e1
Eric Scott <scottericr@gmail.com>
2024-10-07 10:30:44 -0700

    remove old and unused

--- lessons/12-drop-in/01-save_penguins.R
+++
@@ -1,7 +0,0 @@
-library(palmerpenguins)
-
-penguins <- penguins
-
-write.csv(penguins, here::here("multi_language_bash", "penguins_dat.csv"))
-
-print('Saved penguins_dat.csv')
--- lessons/12-drop-in/run.sh
+++
@@ -1,3 +0,0 @@
-Rscript 01-save_penguins.R
-bash 02-rename_penguins.sh
-python 03-print_penguins.py
